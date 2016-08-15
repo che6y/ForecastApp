881b96ea0d9bedeae57488faa9b02dadff31090e
Anastasiia <che6yforever@gmail.com>
2016-08-15 21:31:52 +0700

    BarChart version

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import _ from 'lodash';
-import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines';
+import {Sparklines, SparklinesBars, SparklinesReferenceLine} from 'react-sparklines';
+import { BarChart } from 'react-d3';
 
-function average(data) {
-  return _.round(_.sum(data)/data.length);
-}
+// function average(data) {
+//   return _.round(_.sum(data)/data.length);
+// }
+// export default (props) => {
+//   return (
+//     <div>
+//       <Sparklines height={120} width={180} data={props.data}>
+//         <SparklinesBars style={{ fill: "#41c3f9" }} />
+//         <SparklinesReferenceLine type='avg'/>
+//       </Sparklines>
+//       <div>{average(props.data)} {props.units}</div>
+//     </div>
+//   );
+// }
+
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={180} data={props.data}>
-        <SparklinesLine color={props.color} />
-        <SparklinesReferenceLine type='avg'/>
-      </Sparklines>
-      <div>{average(props.data)} {props.units}</div>
+      <BarChart
+        data={props.barData}
+        width={200}
+        height={100}
+        fill={'#3182bd'}
+      />
     </div>
   );
 }
